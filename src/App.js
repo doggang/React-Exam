4d05fa67ae97c142faa4e20d2962bdaba0d9c6cf
doggang <rlaehgusqp@naver.com>
2023-12-18 23:21:10 +0900

    Git state updating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Article(props){
+  return(
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
+function Header(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <header>
+    <h1>
+      <a href="#" onClick={(e)=>{
+        e.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a>
+    </h1>
+  </header>
+  )
+}
+
+function Nav(props){
+  const lis = [];
+
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'./read/'+t.id} onClick={(e)=>{
+        e.preventDefault();
+        props.onChangeMode(Number(e.target.id));
+      
+      }}>{t.title}</a></li>);
+  }
+
+  return(
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Create(props){
+  return(
+    <h2>Create</h2>
+    <form>
+      <p></p>
+    </form>
+  )
+}
+function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+
+  const topics = [
+    {id: 1, title: 'html', body:"html is..."},
+    {id: 2, title: 'css', body:"css is..."},
+    {id: 3, title: 'js', body:"js is..."}
+  ]
+  let content = null;
+  if(mode === "WELCOME"){
+    content = <Article title="Welcome" body="Hello, WEB"/>
+  }else if(mode === "READ"){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}/>
+  }else if(mode === "Create"){
+    content = 
+  }
+  return (
+    <div>
+      <Header title="REACT" onChangeMode={()=>{
+        setMode("WELCOME");
+      }} />
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode("READ");
+        setId(_id)
+      }}/>
+      {content}
+      <a href='./Create' onClick={(e)=>{
+        e.preventDefault();
+        setMode="Create";
+      }}>Create</a>
+      <Create/>
     </div>
   );
 }
